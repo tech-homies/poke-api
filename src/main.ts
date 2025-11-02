29b992613528a74271212766cb9d26a2ac57daec
Benjamin Barbier <benjamin.barbier@partner.kiabi.com>
2025-11-02 03:41:58 +0100

    🔧 add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
       whitelist: true, // Supprime les propriétés non définies dans les DTOs
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Pokemon API')
+    .setDescription('The Pokemon API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
