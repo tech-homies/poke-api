90b77d00b6f03672df9c722f6b02575c957e4ea6
Benjamin Barbier <benjamin.barbier@partner.kiabi.com>
2025-11-02 03:49:13 +0100

    ✨ add middleware to simulate latency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { PokemonTypesModule } from './pokemon-types/pokemon-types.module';
 import { PokemonsModule } from './pokemons/pokemons.module';
+import { DelayMiddleware } from './middlewares/delay.middleware';
 
 @Module({
   imports: [PokemonTypesModule, PokemonsModule],
   providers: [{ provide: APP_FILTER, useClass: AllExceptionsFilter }],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(DelayMiddleware).forRoutes('types', 'pokemons');
+  }
+}
