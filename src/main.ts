73e600eb53371a4f1b019cc560699b85ac7b6e2c
Benjamin Barbier <benjamin.barbier@partner.kiabi.com>
2025-11-02 02:00:17 +0100

    🔒️ strip object properties that do not use any validation decorators

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // Activation de la validation automatique des DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Supprime les propriétés non définies dans les DTOs
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
