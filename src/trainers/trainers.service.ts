3b5b68b8fd9f2835878c48da2bb773b33e1c7b85
Benjamin Barbier <benjamin.barbier@partner.kiabi.com>
2025-11-02 22:45:28 +0100

    ✨ add method to create trainer

--- src/trainers/trainers.service.ts
+++ src/trainers/trainers.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Trainer } from './entities/trainer.entity';
 import { trainers } from './trainers.data';
+import { CreateTrainerDto } from './dto/create-trainer.dto';
 
 @Injectable()
 export class TrainersService {
@@ -12,5 +13,15 @@ export class TrainersService {
 
   public findOne(id: number): Trainer | undefined {
     return this.trainers.find((trainer) => trainer.id === id);
+  }
+
+  public create(createTrainerDto: CreateTrainerDto): Trainer {
+    const newId = Math.max(...this.trainers.map((t) => t.id), 0) + 1;
+    const newTrainer: Trainer = {
+      id: newId,
+      ...createTrainerDto,
+    };
+    this.trainers = this.trainers.concat(newTrainer);
+    return newTrainer;
   }
 }
